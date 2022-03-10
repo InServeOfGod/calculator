633d6d466d1d83829196db204749a4451ef5a2b6
InServeOfGod <inserveofgod@gmail.com>
2022-03-10 23:17:16 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# calculator
+
+Python tkinter modülü ile mvc tabanlı basit bir 
+GUI (grafiksel kullanıcı arayüzlü) hesap makinesi yapımı
+
+# kütüphaneler
+tkinter, os, sys
---
+++ view.py
@@ -0,0 +1,10 @@
+from tkinter import ttk
+import tkinter as tk
+
+
+class View:
+    def __init__(self, controller):
+        self.controller = controller
+
+    def main(self):
+        pass
